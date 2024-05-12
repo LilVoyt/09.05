6ccdb8bb70ea276bf5b59eb960c5f6d53e84f223
Voytanovskyy <olegvoyt6@gmail.com>
2024-05-12 15:33:54 +0300

    last changes

--- script.js
+++ script.js
@@ -43,65 +43,65 @@
 
 
 //Ex 2 
-  function MyArray() {
-    this.length = 0;
-  }
-  MyArray.prototype.push = function (...args) {
-    for (const v of args) {
-      this[this.length] = v;
-      this.length++;
-    }
-    return this.length;
-  }
-  MyArray.prototype.forEach = function (callback) {
-    for (let i = 0; i < this.length; i++) {
-      callback(this[i], i, this);
-    }
-  }
+  // function MyArray() {
+  //   this.length = 0;
+  // }
+  // MyArray.prototype.push = function (...args) {
+  //   for (const v of args) {
+  //     this[this.length] = v;
+  //     this.length++;
+  //   }
+  //   return this.length;
+  // }
+  // MyArray.prototype.forEach = function (callback) {
+  //   for (let i = 0; i < this.length; i++) {
+  //     callback(this[i], i, this);
+  //   }
+  // }
   
-  MyArray.prototype.filter = function (callback) {
-    const newArr = new MyArray();
-    for (let i = 0; i < this.length; i++) {
-      if (callback(this[i], i, this)) {
-        newArr.push(this[i]);
-      }
-    }
-    return newArr
-  }
+  // MyArray.prototype.filter = function (callback) {
+  //   const newArr = new MyArray();
+  //   for (let i = 0; i < this.length; i++) {
+  //     if (callback(this[i], i, this)) {
+  //       newArr.push(this[i]);
+  //     }
+  //   }
+  //   return newArr
+  // }
 
-  MyArray.prototype.pop = function (){
-    let res = this[this.length];
-    this.length -= 1;
-    delete this[this.length];
+  // MyArray.prototype.pop = function (){
 
-    return res;
-  }
+  //   this.length--;
+  //     const res = this[this.length];
+  //     delete this[this.length]; 
+  //     return res;
+  // }
 
-  MyArray.prototype.some = function(callback) {
-    for (let i = 0; i < this.length; i++) {
-      if (callback(this[i], i, this)) {
-        return true; 
-      }
-    }
-    return false; 
-  }
+  // MyArray.prototype.some = function(callback) {
+  //   for (let i = 0; i < this.length; i++) {
+  //     if (callback(this[i], i, this)) {
+  //       return true; 
+  //     }
+  //   }
+  //   return false; 
+  // }
 
-  MyArray.prototype.every = function(callback) {
-    for (let i = 0; i < this.length; i++) {
-      if (!callback(this[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
+  // MyArray.prototype.every = function(callback) {
+  //   for (let i = 0; i < this.length; i++) {
+  //     if (!callback(this[i])) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
 
-  MyArray.prototype.map = function(callback) {
-    const newArray = new MyArray();
-    for (let i = 0; i < this.length; i++) {
-        newArray.push(callback(this[i], i, this));
-    }
-    return newArray;
-  }
+  // MyArray.prototype.map = function(callback) {
+  //   const newArray = new MyArray();
+  //   for (let i = 0; i < this.length; i++) {
+  //       newArray.push(callback(this[i], i, this));
+  //   }
+  //   return newArray;
+  // }
   
   
   
@@ -114,19 +114,82 @@
 //   console.log(newArr);
 //   console.log(arr.length);
   
-  const myArr = new MyArray();
-  myArr.push(11, 22, 33, 44);
+  // const myArr = new MyArray();
+  // myArr.push(11, 22, 33, 44);
 //   console.log(myArr.push(44));
 //   console.log(myArr);
 //   myArr.forEach((value, index, arr) => console.log(value, index, arr));
 //   const newMyArr = myArr.filter((value) => value % 2 === 0);
 //   console.log(newMyArr);
 //   console.log(myArr.length);
-  myArr.pop();
+  // myArr.pop();
 
-  console.log(myArr.length);
-  console.log(myArr);
-  myArr.push(55);
+  // console.log(myArr.length);
+  // console.log(myArr);
+  // myArr.push(55);
 
-  console.log(myArr.every((element) => element < 40))
-  console.log(myArr.some((element) => element > 60))
+  // console.log(myArr.every((element) => element < 40))
+  // console.log(myArr.some((element) => element > 60))
+
+//Ex 3 
+  class MyArray{
+    constructor(){
+      this.length = 0;
+    }
+    push(...args){
+      for (const i of args) {
+        this[this.length] = i;
+        this.length++;
+      }
+      return this.length;
+    }
+    forEach(callback) {
+      for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+      }
+    }
+    filter(callback) {
+      const newArr = new MyArray();
+      for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+          newArr.push(this[i]);
+        }
+      }
+      return newArr
+    }
+    pop(){
+      this.length--;
+      const res = this[this.length];
+      delete this[this.length]; 
+      return res;
+    }
+    some(callback) {
+      for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+          return true; 
+        }
+      }
+      return false; 
+    }
+    every(callback) {
+      for (let i = 0; i < this.length; i++) {
+        if (!callback(this[i])) {
+          return false;
+        }
+      }
+      return true;
+    }
+    map(callback) {
+      const newArray = new MyArray();
+      for (let i = 0; i < this.length; i++) {
+          newArray.push(callback(this[i], i, this));
+      }
+      return newArray;
+    }
+  }
+
+  const myarr = new MyArray();
+
+  myarr.push(1, 4, 32);
+
+  console.log(myarr);
